37b8c208a028b692b574ce3eb55ffe1a49e11100
smarthug <kirklayer@gmail.com>
2023-09-02 21:55:40 +0900

    Clean up;

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import "./style.css";
-import ReactDOM from "react-dom/client";
-
-import Root from "./Root";
-
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-
-root.render(<Root />);
